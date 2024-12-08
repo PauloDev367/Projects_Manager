14c4af61e93d93bb8992a0d2c4f4ef148840a3d1
Paulo Silva <pauloecomercial367@gmail.com>
2024-12-08 19:53:34 -0300

    :rotating_light: update column tests

--- api/tests/Services/ColumnsServiceTest.php
+++ api/tests/Services/ColumnsServiceTest.php
@@ -6,8 +6,11 @@ use Tests\TestCase;
 use App\Models\User;
 use App\Models\Column;
 use App\Services\ColumnsService;
+use Illuminate\Routing\Redirector;
 use PHPUnit\Framework\MockObject\MockObject;
 use App\Http\Requests\V1\CreateColumnRequest;
+use App\Http\Requests\V1\UpdateColumnRequest;
+use App\Repositories\ColumnRepository;
 use App\Repositories\Ports\IColumnRepository;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
@@ -148,5 +151,72 @@ class ColumnsServiceTest extends TestCase
         $this->expectException(ModelNotFoundException::class);
 
         $columnsService->delete($user, $columnId);
+    }
+    public function test_update_column_not_found()
+    {
+        $user = new User();
+        $user->id = 1; 
+
+        $request = new UpdateColumnRequest([
+            'title' => 'New Title'
+        ]);
+
+        $repository = $this->createMock(ColumnRepository::class);
+        $repository->method('getOne')->willReturn(null);
+
+        $service = new ColumnsService($repository);
+
+        $this->expectException(ModelNotFoundException::class);
+        $this->expectExceptionMessage('Column not founded');
+
+        $service->update($user, 1, $request);
+    }
+
+    public function test_update_column_with_valid_data()
+    {
+        $user = new User();
+        $user->id = 1;
+
+        $column = new Column();
+        $column->id = 1;
+        $column->title = 'Old Title';
+
+        $request = new UpdateColumnRequest([
+            'title' => 'Updated Title'
+        ]);
+
+        $repository = $this->createMock(ColumnRepository::class);
+        $repository->method('getOne')->willReturn($column);
+        $repository->method('update')->willReturn($column);
+
+        $service = new ColumnsService($repository);
+
+        $updatedColumn = $service->update($user, $column->id, $request);
+
+        $this->assertEquals('Updated Title', $updatedColumn->title);
+    }
+
+    public function test_update_column_without_changing_title()
+    {
+        $user = new User();
+        $user->id = 1;
+
+        $column = new Column();
+        $column->id = 1;
+        $column->title = 'Old Title';
+
+        $request = new UpdateColumnRequest([
+            'title' => null
+        ]);
+
+        $repository = $this->createMock(ColumnRepository::class);
+        $repository->method('getOne')->willReturn($column);
+        $repository->method('update')->willReturn($column); 
+
+        $service = new ColumnsService($repository);
+
+        $updatedColumn = $service->update($user, $column->id, $request);
+
+        $this->assertEquals('Old Title', $updatedColumn->title);
     }
 }
